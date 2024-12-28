b8422cadcde260078d18e5c9a6efaf46c2d0c3c6
rk <ritiksaini8060@gmail.com>
2024-12-28 14:43:27 +0530

    EventCategoryList.dart - update event name out of card

--- lib/Screens/HomeScreen/EventCategory/EventCategoryList.dart
+++ lib/Screens/HomeScreen/EventCategory/EventCategoryList.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_projects/Config/Colors.dart';
 
 class EventCategoryList extends StatelessWidget {
   const EventCategoryList({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    // events Card list
     final List<Map<String, dynamic>> events = [
       {
         'name': 'Birthday',
-        // 'imagePath': 'assets/icons/Balloons.png',
         'imagePath': 'assets/icons/Balloons.png',
         'routeName': '/birthdayScreen'
       },
@@ -70,54 +67,239 @@ class EventCategoryList extends StatelessWidget {
         itemCount: events.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 5,
-          crossAxisSpacing: 4,
-          mainAxisSpacing: 4,
+          // crossAxisSpacing: 4,
+          // mainAxisSpacing: 4,
         ),
         itemBuilder: (context, index) {
           final event = events[index];
           return GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, event['routeName']);
-            },
-            child: Column(
-              children: [
-                AspectRatio(
-                  aspectRatio: 1, // ensures square shape
-                  child: Card(
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset(
-                        event['imagePath'],
-                          fit: BoxFit.contain,
+              onTap: () {
+                Navigator.pushNamed(context, event['routeName']);
+              },
+              child: SizedBox(
+                  height: 90,
+                  child: Column(children: [
+                    Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: const EdgeInsets.all(0),
+                        decoration: BoxDecoration(
+                            color: whiteColor,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: whiteColor,
+                              width: 2,
+                            )),
+                        child: Image.asset(
+                          event['imagePath'],
+                          height: 50,
+                          width: 50,
+                          // fit: BoxFit.contain,
+                        ),
                       ),
-                      // Image.asset(
-                      //   event['imagePath'],
-                      //   fit: BoxFit.contain,
-                      // ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  event['name'],
-                  style: const TextStyle(
-                      fontSize: 12, fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          );
+                    const SizedBox(height: 2),
+                    Text(
+                      event['name'],
+                      style: const TextStyle(
+                          fontSize: 11, fontWeight: FontWeight.w900),
+                      textAlign: TextAlign.center,
+                    ),
+                  ])));
         },
       ),
     );
   }
 }
 
+//------------------------------------------------------------------------------
+// class EventCategoryList extends StatelessWidget {
+//   const EventCategoryList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<Map<String, dynamic>> events = [
+//       {'name': 'Birthday', 'imagePath': 'assets/icons/Balloons.png', 'routeName': '/birthdayScreen'},
+//       {'name': 'Engagement', 'imagePath': 'assets/icons/Ring.png', 'routeName': '/engagementScreen'},
+//       {'name': 'Anniversary', 'imagePath': 'assets/icons/Gift.png', 'routeName': '/anniversaryScreen'},
+//       {'name': 'Wedding', 'imagePath': 'assets/icons/weddingCouple.png', 'routeName': '/weddingScreen'},
+//       {'name': 'Pre Wedding', 'imagePath': 'assets/icons/preWedding.png', 'routeName': '/preWeddingScreen'},
+//       {'name': 'Meetup', 'imagePath': 'assets/icons/community.png', 'routeName': '/meetupScreen'},
+//       {'name': 'Charity', 'imagePath': 'assets/icons/charity.png', 'routeName': '/charityScreen'},
+//       {'name': 'Reunions', 'imagePath': 'assets/icons/familyGathering.png', 'routeName': '/reunionsScreen'},
+//       {'name': 'Corporate', 'imagePath': 'assets/icons/coorportate.png', 'routeName': '/corporateScreen'},
+//       {'name': 'Others', 'imagePath': 'assets/icons/others.png', 'routeName': '/othersScreen'},
+//     ];
+//
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 10.0),
+//       child: GridView.builder(
+//         shrinkWrap: true,
+//         physics: const NeverScrollableScrollPhysics(),
+//         itemCount: events.length,
+//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 5,
+//           crossAxisSpacing: 4,
+//           mainAxisSpacing: 4,
+//         ),
+//         itemBuilder: (context, index) {
+//           final event = events[index];
+//           return GestureDetector(
+//             onTap: () {
+//               Navigator.pushNamed(context, event['routeName']);
+//             },
+//             child: Card(
+//               elevation: 2,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(8),
+//               ),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Expanded(
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         image: DecorationImage(
+//                           image: AssetImage(event['imagePath']),
+//                           fit: BoxFit.contain,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 8),
+//                   Text(
+//                     event['name'],
+//                     style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+//                     textAlign: TextAlign.center,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
 
+// -----------------------------------------------------------------------------
+// class EventCategoryList extends StatelessWidget {
+//   const EventCategoryList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     // events Card list
+//     final List<Map<String, dynamic>> events = [
+//       {
+//         'name': 'Birthday',
+//         'imagePath': 'assets/icons/Balloons.png',
+//         'routeName': '/birthdayScreen'
+//       },
+//       {
+//         'name': 'Engagement',
+//         'imagePath': 'assets/icons/Ring.png',
+//         'routeName': '/engagementScreen'
+//       },
+//       {
+//         'name': 'Anniversary',
+//         'imagePath': 'assets/icons/Gift.png',
+//         'routeName': '/anniversaryScreen'
+//       },
+//       {
+//         'name': 'Wedding',
+//         'imagePath': 'assets/icons/weddingCouple.png',
+//         'routeName': '/weddingScreen'
+//       },
+//       {
+//         'name': 'Pre Wedding',
+//         'imagePath': 'assets/icons/preWedding.png',
+//         'routeName': '/preWeddingScreen'
+//       },
+//       {
+//         'name': 'Meetup',
+//         'imagePath': 'assets/icons/community.png',
+//         'routeName': '/meetupScreen'
+//       },
+//       {
+//         'name': 'Charity',
+//         'imagePath': 'assets/icons/charity.png',
+//         'routeName': '/charityScreen'
+//       },
+//       {
+//         'name': 'Reunions',
+//         'imagePath': 'assets/icons/familyGathering.png',
+//         'routeName': '/reunionsScreen'
+//       },
+//       {
+//         'name': 'Corporate',
+//         'imagePath': 'assets/icons/coorportate.png',
+//         'routeName': '/corporateScreen'
+//       },
+//       {
+//         'name': 'Others',
+//         'imagePath': 'assets/icons/others.png',
+//         'routeName': '/othersScreen'
+//       },
+//     ];
+//
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 10.0),
+//       child: GridView.builder(
+//         shrinkWrap: true,
+//         physics: const NeverScrollableScrollPhysics(),
+//         // itemCount: events.length,
+//         itemCount: 5,
+//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 5,
+//           crossAxisSpacing: 4,
+//           mainAxisSpacing: 4,
+//         ),
+//         itemBuilder: (context, index) {
+//           final event = events[index];
+//           return GestureDetector(
+//             onTap: () {
+//               Navigator.pushNamed(context, event['routeName']);
+//             },
+//             child: Container(
+//               color: Colors.red,
+//               height: 90,
+//               child: Column(
+//                 children: [
+//                   Container(
+//
+//                     // elevation: 2,
+//                     // shape: RoundedRectangleBorder(
+//                     //   borderRadius: BorderRadius.circular(8),
+//
+//                     child: Padding(
+//                       padding: const EdgeInsets.all(0),
+//                       child: Image.asset(
+//                         event['imagePath'],
+//                         height: 20,
+//                         width: 20,
+//                         // fit: BoxFit.contain,
+//                       ),
+//                     ),
+//                   ),
+//                   // const SizedBox(height: 8),
+//                   Text(
+//                     event['name'],
+//                     style: const TextStyle(
+//                         fontSize: 12, fontWeight: FontWeight.w500),
+//                     textAlign: TextAlign.center,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+// -----------------------------------------------------------------------------
 // import 'package:flutter/material.dart';
 // import 'package:flutter_projects/Config/Images.dart';
 // import 'CategoryCard.dart';
